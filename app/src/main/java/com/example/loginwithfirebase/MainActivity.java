e839114cee8dcd05f1eccc174c5f57aafb0de745
Shuvo Talukdar <shuvo6904@gmail.com>
2021-01-29 00:35:43 +0600

    Add multiple value

--- app/src/main/java/com/example/loginwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/loginwithfirebase/MainActivity.java
@@ -14,6 +14,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button logoutButton;
@@ -35,7 +37,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FirebaseDatabase.getInstance().getReference().child("TestBranch").child("SubTestBranch").setValue("Android");
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("Name", "Shuvo");
+        map.put("Email", "shuvo6904@gmail.com");
+
+        FirebaseDatabase.getInstance().getReference().child("TestBranch").child("Multiplevalue").updateChildren(map);
 
     }
 
