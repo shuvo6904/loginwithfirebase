1953b3bc66e07cf8f161d0d821412d7c26b64c44
Shuvo Talukdar <shuvo6904@gmail.com>
2021-01-28 13:47:19 +0600

    Logged Out from Login Page

--- app/src/main/java/com/example/loginwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/loginwithfirebase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.loginwithfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +15,24 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoutButton = findViewById(R.id.logoutButtonId);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out !", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+        });
 
 
     }
